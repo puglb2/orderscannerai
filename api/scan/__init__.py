8fb4bfb989cb6b9e5892c50ed4b3960e7dcd7e41
puglb2 <puglb2@gmail.com>
2026-01-23 10:12:51 -0700

    Delete api directory

--- api/scan/__init__.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-import json
-
-def main(req: func.HttpRequest, context: func.Context):
-    return func.HttpResponse(
-        json.dumps({"status": "ok"}),
-        status_code=200,
-        mimetype="application/json"
-    )
